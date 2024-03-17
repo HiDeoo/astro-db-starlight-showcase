675f2fc907f834b4f80edc3438205ea1354b1932
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-03-17 18:08:54 +0100

    feat: add basic dashboard

--- src/libs/schema.ts
+++ src/libs/schema.ts
@@ -1,7 +1,12 @@
 import { z } from 'astro/zod'
 import { zfd } from 'zod-form-data'
 
-export const newShowcaseEntrySchema = zfd.formData({
+export const addShowcaseEntrySchema = zfd.formData({
   url: zfd.text(z.string().url()),
-  name: zfd.text(),
+  name: zfd.text(z.string().min(200)),
+})
+
+export const deleteShowcaseEntrySchema = zfd.formData({
+  action: zfd.text(z.literal('delete')),
+  id: zfd.text(z.string().uuid()),
 })
--- src/libs/showcase.ts
+++ src/libs/showcase.ts
@@ -1,9 +1,25 @@
-import { db, ShowcaseEntry, ShowcaseUser } from 'astro:db'
+import { and, db, desc, eq, ShowcaseEntry, ShowcaseUser } from 'astro:db'
 
-export function addShowcaseEntry(user: typeof ShowcaseUser.$inferSelect, url: string, name: string) {
+export function getApprovedShowcaseEntries() {
+  // TODO(HiDeoo) public
+  return db.select().from(ShowcaseEntry).orderBy(desc(ShowcaseEntry.createdAt))
+}
+
+export function addShowcaseEntry(user: ShowcaseUser, url: string, name: string) {
   return db.insert(ShowcaseEntry).values({
     userId: user.id,
     url: url,
     name: name,
   })
 }
+
+export function getUserShowcaseEntries(user: ShowcaseUser) {
+  return db.select().from(ShowcaseEntry).where(eq(ShowcaseEntry.userId, user.id)).orderBy(desc(ShowcaseEntry.createdAt))
+}
+
+export function deleteUsersShowcaseEntry(user: ShowcaseUser, id: string) {
+  return db.delete(ShowcaseEntry).where(and(eq(ShowcaseEntry.userId, user.id), eq(ShowcaseEntry.id, id)))
+}
+
+type ShowcaseUser = typeof ShowcaseUser.$inferSelect
+export type ShowcaseEntry = typeof ShowcaseEntry.$inferSelect
